2149f262c198951171e12bc9ebb8683b63b47bba
Toasty <105026195+JackReynoldsjk@users.noreply.github.com>
2025-07-11 06:56:09 -0400

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Snorff | TLDR Development'
+description 'Dino Game Loading Screen'
+version '1.0.1'
+
+loadscreen 'index.html'
+
+files {
+  'index.html',
+  'index.css',
+  'index.js',
+  'style.css',
+  'assets/**/*'
+}
